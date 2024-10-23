b4426fed059f620165030e5630ca156f328821cd
kundusaab <kunduanubhav@icloud.com>
2024-10-23 15:14:10 +0530

    Update script1.js

--- 21102024/script1.js
+++ 21102024/script1.js
@@ -10,23 +10,31 @@ console.log(lastLetterOfLastName);
 
 var firstName = "ramniwas";
 var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
-console.log(thirdToLastLetterOfFirstName);  // Correct log for third-to-last letter
+console.log(thirdToLastLetterOfFirstName); // Correct log for third-to-last letter
 
 var lastName = "rohtak";
 var secondToLastLetterOfLastName = lastName[lastName.length - 2];
-console.log(secondToLastLetterOfLastName);  // Correct log for second-to-last letter
+console.log(secondToLastLetterOfLastName); // Correct log for second-to-last letter
 
-
-// word blanks 
+// word blanks
 
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
-    var result = "";
-    result += "The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb + ".";
-    return result;
+  var result = "";
+  result +=
+    "The " +
+    myAdjective +
+    " " +
+    myNoun +
+    " " +
+    myVerb +
+    " to the store " +
+    myAdverb +
+    ".";
+  return result;
 }
 
-console.log(wordBlanks("dog", "big", "ran", "quickly"));   // The big dog ran to the store quickly.
-console.log(wordBlanks("bike", "slow", "flew", "slowly"));  // The slow bike flew to the store slowly.
+console.log(wordBlanks("dog", "big", "ran", "quickly")); // The big dog ran to the store quickly.
+console.log(wordBlanks("bike", "slow", "flew", "slowly")); // The slow bike flew to the store slowly.
 
 // store multiple values with arrays
 var ourArray = ["John", 23];
@@ -34,27 +42,60 @@ var myArray = ["Quincy", 1];
 console.log(ourArray);
 console.log(myArray);
 
-// nested arrays 
+// nested arrays
 
-    var ourArray = [["The universe",42 ], ["everything", 101010]];
-    var myArray = [["bulls", 23], ["white sox, 26"]];
-    console.log(ourArray);
-    console.log(myArray); 
+var ourArray = [
+  ["The universe", 42],
+  ["everything", 101010],
+];
+var myArray = [["bulls", 23], ["white sox, 26"]];
+console.log(ourArray);
+console.log(myArray);
 
-    // Array finding 
-    var ourArray = [50, 60, 70];
+// Array finding
+var ourArray = [50, 60, 70];
 
-    var ourData = ourArray[0];
+var ourData = ourArray[0];
 
-    var myData =myArray[50,60,70];
-    var myData = myArray[0];
-    console.log(myData);
-    console.log(myArray);
+var myData = myArray[(50, 60, 70)];
+var myData = myArray[0];
+console.log(myData);
+console.log(myArray);
 
-    var ourArray =[1,2,3];
-    var removedFromOurArray = ourArray.pop();
-    console.log(ourArray);
+var ourArray = [1, 2, 3];
+var removedFromOurArray = ourArray.pop();
+console.log(ourArray);
 
-    var myArray = [["John", 23], ["Cat", 2]];
-    var removedFromOurArray = myArray.pop();
-    console.log(myArray);
+var myArray = [
+  ["John", 23],
+  ["Cat", 2],
+];
+var removedFromOurArray = myArray.pop();
+console.log(myArray);
+
+// manpulation arrays with shift()
+var ourArray = ["Stimpson", "J", ["Cat"]];
+var removedFromOurArray = ourArray.shift();
+
+var myArray = [
+  ["John", 23],
+  ["Dog", 3],
+];
+newFunction();
+
+function newFunction() {
+  var removedFromMyArray = myArray.shift();
+}
+console.log(myArray);
+
+// manpulatio arrays with unshift()
+var ourArray = ["Stimpson", "J", ["Cat"]];
+ourArray.shift = ourArray.shift();
+ourArray.unshift = ("happy")
+var myArray = [["John", 23],["cat", 3],];
+newFunction();
+
+function newFunction() {
+  var removedFromMyArray = myArray.pop();
+}
+console.log(myArray);
